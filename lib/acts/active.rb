e58e516987781ef4bfba1e689a2a8c2f7332286d
Fernando Luizao <fernandoluizao@gmail.com>
2009-03-07 07:16:56 -0300

    added the capability of use default_scope with the plugin

--- lib/acts/active.rb
+++ lib/acts/active.rb
@@ -15,6 +15,25 @@ module Acts #:nodoc:
           #show only active
           default_scope :conditions => {self.active_attribute => true}
 
+          class << self
+            #override the default_scope to inject the active attribute
+            alias_method :default_scope_without_active, :default_scope
+  
+            def default_scope(scope_options)
+              scope_options[:conditions] ||= {}
+              if scope_options[:conditions].is_a? Hash
+                scope_options[:conditions].update(self.active_attribute => true)
+              elsif scope_options[:conditions].is_a? Array
+                scope_options[:conditions][0] = inject_active_in_condition(scope_options[:conditions][0])
+              elsif scope_options[:conditions].is_a? String
+                scope_options[:conditions] = inject_active_in_condition(scope_options[:conditions])
+              else
+                raise "what did you pass as conditions??"
+              end
+              default_scope_without_active(scope_options)
+            end
+          end
+
           #copy the :destroy_without_callbacks
           alias_method :destroy_without_callbacks!, :destroy_without_callbacks
           include InstanceMethods
@@ -36,7 +55,11 @@ module Acts #:nodoc:
       def count_with_inactive(*args)
         with_exclusive_scope { count(*args) }
       end
-      
+
+      private
+      def inject_active_in_condition(condition)
+        "(#{condition}) AND (#{self.active_attribute} = #{quote_value(true)})"
+      end
     end
 
     module InstanceMethods
